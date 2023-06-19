8a9c254408278c2a4f7f9cf483703c315597ef76
samudiSenevirathne <stsamudi@gmail.com>
2023-06-19 23:22:54 +0530

    Content Updated

--- myScript.js
+++ myScript.js
@@ -4,5 +4,13 @@ $("#certificateOne").click(function () {
        var backgroundUrl2 = $("#certificateFive").css('background-image');
        $("#certificateFive").css('background-image', backgroundUrl);
         $("#certificateOne").css('background-image', backgroundUrl2);
-        console.log(backgroundUrl);
+        // console.log(backgroundUrl); //for my checking
+});
+
+$("#certificateTwo").click(function () {
+    var backgroundUrl = $("#certificateTwo").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateTwo").css('background-image', backgroundUrl2);
+    // console.log(backgroundUrl); //for my checking
 });
