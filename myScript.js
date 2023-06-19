0ff96035d7b288860cd6f1ef2445e4fbe03ea5db
samudiSenevirathne <stsamudi@gmail.com>
2023-06-19 23:42:42 +0530

    Content Updated

--- myScript.js
+++ myScript.js
@@ -1,10 +1,10 @@
-
 $("#certificateOne").click(function () {
        var backgroundUrl = $("#certificateOne").css('background-image');
        var backgroundUrl2 = $("#certificateFive").css('background-image');
        $("#certificateFive").css('background-image', backgroundUrl);
         $("#certificateOne").css('background-image', backgroundUrl2);
         // console.log(backgroundUrl); //for my checking
+       // console.log(backgroundUrl2); //for my checking
 });
 
 $("#certificateTwo").click(function () {
@@ -12,5 +12,46 @@ $("#certificateTwo").click(function () {
     var backgroundUrl2 = $("#certificateFive").css('background-image');
     $("#certificateFive").css('background-image', backgroundUrl);
     $("#certificateTwo").css('background-image', backgroundUrl2);
-    // console.log(backgroundUrl); //for my checking
+});
+
+$("#certificateThree").click(function () {
+    var backgroundUrl = $("#certificateThree").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateThree").css('background-image', backgroundUrl2);
+});
+
+$("#certificateFour").click(function () {
+    var backgroundUrl = $("#certificateFour").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateFour").css('background-image', backgroundUrl2);
+});
+
+$("#certificateSix").click(function () {
+    var backgroundUrl = $("#certificateSix").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateSix").css('background-image', backgroundUrl2);
+});
+
+$("#certificateSeven").click(function () {
+    var backgroundUrl = $("#certificateSeven").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateSeven").css('background-image', backgroundUrl2);
+});
+
+$("#certificateEight").click(function () {
+    var backgroundUrl = $("#certificateEight").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateEight").css('background-image', backgroundUrl2);
+});
+
+$("#certificateNine").click(function () {
+    var backgroundUrl = $("#certificateNine").css('background-image');
+    var backgroundUrl2 = $("#certificateFive").css('background-image');
+    $("#certificateFive").css('background-image', backgroundUrl);
+    $("#certificateNine").css('background-image', backgroundUrl2);
 });
